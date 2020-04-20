e549559eef2843642b283c0767d2d4aa25828d7b
Tamara <tami_noeps@hotmail.com>
2020-04-20 17:41:11 +0200

    Archivos Laravel de Modelo

---
+++ Documentacion/Entrega 2/Archivos Laravel de Modelo/2020_04_05_102344_create_favorites_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('user_id_favorite');
+            $table->foreign('user_id_favorite')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->primary(['user_id', 'user_id_favorite']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
